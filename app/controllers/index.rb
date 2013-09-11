87cb7cee2cd094e4c5b42db2b1626c954a3766ea
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-10 19:40:09 -0500

    first

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,9 +17,33 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+   @user = User.create(:username => @access_token.params[:screen_name] ,:oauth_token =>@access_token.token , :oauth_secret => @access_token.secret)
   # at this point in the code is where you'll need to create your user account and store the access token
 
+  session[:id]= @user.id
+ 
   erb :index
-  
+ 
 end
+
+post '/tweet' do
+
+  user  = User.find(session[:id])
+
+  Twitter.configure do |config|
+    config.oauth_token = user.oauth_token
+    config.oauth_token_secret = user.oauth_secret
+  end
+
+
+
+  @tweet = Twitter.update(params[:tweet])
+  if request.xhr?
+    erb :thankyou, layout: false
+  else
+    erb :thankyou
+  end
+end
+
+
+
---
+++ twitter_creds.sh
@@ -0,0 +1,4 @@
+# source twitter_creds.sh
+export TWITTER_SECRET='z2rugxkkhlw5JKLqp9Q6PUcrBCaEvuJWpdBSZvIToR4'
+export TWITTER_KEY='JVAglSlXJi3MethO4qyGOQ'
+
